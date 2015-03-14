381e6539e2a2f5047bbab59c733d3dd4b178faaf
Kristen M. Thyng <kthyng@gmail.com>
2015-03-14 10:55:03 -0500

    added an init file

---
+++ cmtamu/__init__.py
@@ -0,0 +1,12 @@
+'''
+cmtamu is a package to help standardize colormaps for commonly-plotted
+oceanographic properties in the Oceanography department at Texas A&M. 
+
+See README.md for an overview on instructions.
+'''
+
+import cmtamu
+import test
+import tools
+
+__authors__ = ['Kristen Thyng <kthyng@tamu.edu>']
