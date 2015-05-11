cea2a1fe00275354c0c8b9a37d63afbb74b882aa
Kristen M. Thyng <kthyng@gmail.com>
2015-05-11 14:21:19 -0500

    tweaked temp colormap to be less bendy in lightness, oxygen to have more gradient, added PAR and density, switched turbidity, added some units

--- cmtamu/cmtamu.py
+++ cmtamu/cmtamu.py
@@ -17,19 +17,19 @@ def make_salinity_cmap():
     return cmap
 
 def make_temperature_cmap():
-    rgb = np.array([[49,54,149], [61,91,167], [245,117,71], [254,235,161]])
+    rgb = np.array([[20,30,120], [55,85,161], [245,117,71], [254,235,161]])
     cmap = test.cmap(rgb)
     cmap.name = 'Temperature'
     cmap.units = 'C'
     cmap.author = 'kmt'
     return cmap
 
-def make_oxygen_cmap(hypo_low  = [0.39, 0.0, 0.15],
-                hypo_high = [0.5, 0.28, 0.31],
-                norm_low  = [0.35, 0.35, 0.35],
-                norm_high = [0.94, 0.94, 0.95], bpt=0.2,
-                sup_low  = [1.0, 1.0, 0.4],
-                sup_high = [0.73, 0.87, 0.4], bpth=0.8):
+def make_oxygen_cmap(hypo_low  = [0.30, 0.1, 0.1],
+                hypo_high = [0.6, 0.1, 0.1],
+                norm_low  = [0.3, 0.3, 0.3],
+                norm_high = [0.80, 0.80, 0.80], bpt=0.2,
+                sup_high  = [1.0, 1.0, 0.4],
+                sup_low = [0.73, 0.85, 0.4], bpth=0.8):
     '''
     The colormap has hue breaks for hypoxic, normal, and supersaturated levels 
     of oxygen. Reasonable lightness functions though.
@@ -50,28 +50,46 @@ def make_oxygen_cmap(hypo_low  = [0.39, 0.0, 0.15],
                        (1.0, sup_high[2], 1.0))}
 
     cmap = colors.LinearSegmentedColormap('Oxygen', cdict, 256)
-    cmap.units = '?'
+    cmap.units = 'm/l'
     cmap.author = 'rdh'
     return cmap
 
 def make_chlorophyll_cmap():
     cmap = cm.Greens
     cmap.name = 'Chlorophyll'
-    cmap.units = '?'
+    cmap.units = 'mg/m^3'
     cmap.author = 'kmt'
     return cmap
 
 def make_CDOM_cmap():
     cmap = cm.RdPu
     cmap.name = 'CDOM'
-    cmap.units = '?'
+    cmap.units = 'mg/m^3'
     cmap.author = 'kmt'
     return cmap
 
 def make_turbidity_cmap():
-    cmap = cm.YlOrBr
+    cmap = test.cmap(['#FEEBB7', '#B0911E', '#4F3F18'])
+    # cmap = cm.YlOrBr
     cmap.name = 'Turbidity'
     cmap.units = '?'
+    cmap.author = 'kmt'
+    return cmap
+
+def make_PAR_cmap():
+    # cmap = test.cmap(['#7B6316', '#A37F13', '#BF910C', '#FCB413', '#FFCC48', '#F8DF8C', '#F7E7AA'])
+    # cmap = test.cmap(['#685739', '#8C7440', '#B89A5D', '#C6AB72', '#DAC496', '#E5D2AA', '#F3E8CB'])
+    # cmap = test.cmap(['#4F3F18', '#B0911E', '#FEEBB7'])
+    cmap = cm.YlOrBr_r
+    cmap.name = 'PAR'
+    cmap.units = 'W/m^2'
+    cmap.author = 'kmt'
+    return cmap
+
+def make_density_cmap():
+    cmap = cm.BuPu
+    cmap.name = 'Density'
+    cmap.units = 'kg/m^3'
     cmap.author = 'kmt'
     return cmap
 
@@ -88,6 +106,10 @@ cdom = make_CDOM_cmap()
 CDOM = make_CDOM_cmap()
 turbidity = make_turbidity_cmap()
 turb = make_turbidity_cmap()
+PAR = make_PAR_cmap()
+par = make_PAR_cmap()
+density = make_density_cmap()
+rho = make_density_cmap()
 
 
 
