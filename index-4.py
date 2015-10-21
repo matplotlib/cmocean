ae26cf4a288b2bab41b7c75edb2585b921350a64
Kristen M. Thyng <kthyng@gmail.com>
2015-10-21 16:37:55 -0500

    added PAR and phase descriptions

--- index-4.py
+++ index-4.py
@@ -4,6 +4,6 @@ import numpy as np
 
 azimuths = np.arange(0, 360, 1)
 zeniths = np.arange(40, 70, 1)
-values = azimuths * ones((30, 361))
+values = azimuths * np.ones((30, 361))
 fig, ax = plt.subplots(subplot_kw=dict(projection='polar'))
 ax.pcolormesh(azimuths*pi/180.0, zeniths, values, cmap=cmocean.cm.phase)
