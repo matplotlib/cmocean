f495e82eb0955dd4191d2546e0cd155114d7e7a6
Kristen M. Thyng <kthyng@gmail.com>
2016-06-19 11:00:08 -0500

    fixed up plot scripts

--- cmocean/plots.py
+++ cmocean/plots.py
@@ -9,28 +9,6 @@ import numpy as np
 import matplotlib as mpl
 
 from . import cm
-from . import tools
-
-
-mpl.rcParams.update({'font.size': 14})
-mpl.rcParams['font.sans-serif'] = 'Arev Sans, Bitstream Vera Sans, Lucida Grande, Verdana, Geneva, Lucid, Helvetica, Avant Garde, sans-serif'
-mpl.rcParams['mathtext.fontset'] = 'custom'
-mpl.rcParams['mathtext.cal'] = 'cursive'
-mpl.rcParams['mathtext.rm'] = 'sans'
-mpl.rcParams['mathtext.tt'] = 'monospace'
-mpl.rcParams['mathtext.it'] = 'sans:italic'
-mpl.rcParams['mathtext.bf'] = 'sans:bold'
-mpl.rcParams['mathtext.sf'] = 'sans'
-mpl.rcParams['mathtext.fallback_to_cm'] = 'True'
-
-# import pdb; pdb.set_trace()
-# # list of colormaps for several functions
-# cmaps = [cmocean.temp, cmocean.o2, cmocean.salinity, cmocean.chl,
-#          cmocean.rho, cmocean.par, cmocean.turb, cmocean.cdom]
-            # cmocean.bathy, cmocean.s, cmocean.v, cmocean.vort,
-            # cmocean.eta]
-
-cmaps = cm.cmap_d
 
 
 def plot_lightness(saveplot=False):
@@ -46,17 +24,19 @@ def plot_lightness(saveplot=False):
 
     fig = plt.figure(figsize=(16, 6))
     ax = fig.add_subplot(111)
-    ax.set_xlim(-0.1, len(cmaps) + 0.1)
+    ax.set_xlim(-0.1, len(cm.cmap_d)/2. + 0.1)
     ax.set_ylim(0, 100)
-    ax.set_xlabel('Lightness for each colormap')
+    ax.set_xlabel('Lightness for each colormap', fontsize=14)
 
-    for j, cmap in enumerate(cmaps):
+    for j, cmapname in enumerate(cm.cmapnames):
+
+        if '_r' in cmapname:  # skip reversed versions for plot
+            continue
+
+        cmap = cm.cmap_d[cmapname]  # get the colormap instance
         rgb = cmap(x)[np.newaxis, :, :3]
         lab = cspace_converter("sRGB1", "CAM02-UCS")(rgb)
-        # lab = color.rgb2lab(rgb)
         L = lab[0, :, 0]
-        # import pdb; pdb.set_trace()
-        # L = lab[0, :, 0]
         if L[-1] > L[0]:
             ax.scatter(x+j*dc, L, c=x, cmap=cmap, s=300, linewidths=0.)
         else:
@@ -67,7 +47,7 @@ def plot_lightness(saveplot=False):
     ax.xaxis.set_ticks_position('top')
     ticker = mpl.ticker.FixedLocator(locs)
     ax.xaxis.set_major_locator(ticker)
-    formatter = mpl.ticker.FixedFormatter([cmap.name for cmap in cmaps])
+    formatter = mpl.ticker.FixedFormatter([cmapname for cmapname in cm.cmapnames])
     ax.xaxis.set_major_formatter(formatter)
     labels = ax.get_xticklabels()
     for label in labels:
@@ -90,29 +70,25 @@ def plot_gallery(saveplot=False):
 
     from colorspacious import cspace_converter
 
-    # don't have reverse colormaps built in yet
-    rgb = tools.print_colormaps([cm.gray], returnrgb=True)
-    gcmap = tools.cmap(rgb[::-1, :])
-
     gradient = np.linspace(0, 1, 256)
     gradient = np.vstack((gradient, gradient))
     x = np.linspace(0.0, 1.0, 256)
 
-    fig, axes = plt.subplots(nrows=len(cmaps), ncols=1, figsize=(6, 12))
+    fig, axes = plt.subplots(nrows=len(cm.cmap_d)/2, ncols=1, figsize=(6, 12))
     fig.subplots_adjust(top=0.99, bottom=0.01, left=0.2, right=0.99, wspace=0.05)
-    # fig.suptitle('Oceanography colormaps', fontsize=16, y=1.0, x=0.6)
 
-    for ax, cmap in zip(axes, cmaps):
+    for ax, cmapname in zip(axes, cm.cmapnames):
+
+        if '_r' in cmapname:  # skip reversed versions for plot
+            continue
+
+        cmap = cm.cmap_d[cmapname]  # get the colormap instance
 
         rgb = cmap(x)[np.newaxis, :, :3]
 
         # Find a good conversion to grayscale
         jch = cspace_converter("sRGB1", "CAM02-UCS")(rgb)  # Not sure why to use JCh instead so using this.
-        # jch = cspace_converter("sRGB1", "JCh")(rgb)
         L = jch[0, :, 0]
-        # # Get colormap in CIE LAB. We want the L here.
-        # lab = color.rgb2lab(rgb)
-        # L = lab[0, :, 0]
         L = np.float32(np.vstack((L, L, L)))
 
         ax.imshow(gradient, aspect='auto', cmap=cmap)
@@ -121,7 +97,7 @@ def plot_gallery(saveplot=False):
         pos2 = [pos1.x0, pos1.y0,  pos1.width, pos1.height / 3.0]
         axbw = fig.add_axes(pos2)  # colorbar axes
         axbw.set_axis_off()
-        axbw.imshow(L, aspect='auto', cmap=gcmap, vmin=0, vmax=100.)
+        axbw.imshow(L, aspect='auto', cmap=cm.gray, vmin=0, vmax=100.)
         pos = list(ax.get_position().bounds)
         x_text = pos[0] - 0.01
         y_text = pos[1] + pos[3]/2.
@@ -173,16 +149,14 @@ def test(cmap, fig=None, ax=None):
 
     # will plot colormap and lightness
     rgb = cmap(x)[np.newaxis, :, :3]
-    # rgb = cm.get_cmap(cmap)(x)[np.newaxis,:,:3]
     lab = cspace_converter("sRGB1", "CAM02-UCS")(rgb)
-    # lab = color.rgb2lab(rgb)
 
     if ax is None:
         fig = plt.figure()
         ax = fig.add_subplot(111)
     ax.scatter(x, lab[0, :, 0], c=x, cmap=cmap, s=300, linewidths=0.)
-    ax.set_title(cmap.name)
-    ax.set_ylabel('Lightness')
+    ax.set_title(cmap.name, fontsize=14)
+    ax.set_ylabel('Lightness', fontsize=14)
     ax.set_xticks([])
 
 
@@ -198,7 +172,7 @@ def quick_plot(cmap, fname=None, fig=None, ax=None, N=10):
         fig = plt.figure()
         ax = fig.add_subplot(111)
     mappable = ax.pcolor(X, cmap=cmap)
-    ax.set_title(cmap.name)
+    ax.set_title(cmap.name, fontsize=14)
     ax.set_xticks([])
     ax.set_yticks([])
     plt.colorbar(mappable)
