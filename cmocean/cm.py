876597254d87271161fc107d17fb818454b32cd8
Kristen M. Thyng <kthyng@gmail.com>
2016-06-19 11:00:57 -0500

    cleaned up cm definition code

--- cmocean/cm.py
+++ cmocean/cm.py
@@ -10,7 +10,6 @@ Created by Kristen Thyng
 
 from __future__ import absolute_import
 
-import matplotlib
 import numpy as np
 import os
 
@@ -20,21 +19,20 @@ from . import tools
 datadir = os.path.join(os.path.split(__file__)[0], 'rgb')
 
 # List of colormap names
-cmaps = ['thermal', 'haline', 'solar', 'ice', 'gray', 'oxy', 'deep', 'dense',
-         'algae', 'matter', 'turbid', 'speed', 'amp', 'tempo', 'phase',
-         'balance', 'delta', 'curl']
+cmapnames = ['thermal', 'haline', 'solar', 'ice', 'gray', 'oxy', 'deep',
+             'dense', 'algae', 'matter', 'turbid', 'speed', 'amp', 'tempo',
+             'phase', 'balance', 'delta', 'curl']
 
 # initialize dictionary to contain colormaps
 cmap_d = dict()
 
 # add colormaps and reversed to dictionary
-for cmap in cmaps:
-    # rgb = np.loadtxt(cmap + '-rgb.txt')
-    rgb = np.loadtxt(os.path.join(datadir, cmap + '-rgb.txt'))
-    cmap_d[cmap] = tools.cmap(rgb, N=256)
-    cmap_d[cmap].name = cmap
-    cmap_d[cmap + '_r'] = tools.cmap(rgb[::-1, :], N=256)
-    cmap_d[cmap + '_r'].name = cmap + '_r'
+for cmapname in cmapnames:
+    rgb = np.loadtxt(os.path.join(datadir, cmapname + '-rgb.txt'))
+    cmap_d[cmapname] = tools.cmap(rgb, N=256)
+    cmap_d[cmapname].name = cmapname
+    cmap_d[cmapname + '_r'] = tools.cmap(rgb[::-1, :], N=256)
+    cmap_d[cmapname + '_r'].name = cmapname + '_r'
 
 # make colormaps available to call
 locals().update(cmap_d)
