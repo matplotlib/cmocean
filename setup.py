edbb9c0f13ea680d0e2bc380d868d845794af83a
Kristen Thyng <kthyng@gmail.com>
2024-04-05 16:34:54 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ extras_require={
 
 setup(
     name = "cmocean",
-    version = "v4.0.1",
+    version = "v4.0.0",
     author = "Kristen Thyng",
     author_email = "kthyng@gmail.com",
     url = 'https://github.com/matplotlib/cmocean',
